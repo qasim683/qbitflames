8568f976d96bc5d3b8dddd2468dd27f06f9a3a3d
qasim683 <envoyfacilitation@outlook.com>
2020-06-29 21:56:47 +0500

    uwsgi conf

--- apache2/wsgi.py
+++ apache2/wsgi.py
@@ -1,22 +1,14 @@
-import os 
-import time 
-import traceback 
-import signal 
-import sys 
- 
-from django.core.wsgi import get_wsgi_application 
- 
-sys.path.append('/var/www/django_apache2') 
-# adjust the Python version in the line below as needed 
-sys.path.append('/var/www/django_apache2/env') 
- 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qbit.settings") 
- 
-try: 
-    application = get_wsgi_application() 
-except Exception: 
-    # Error loading applications 
-    if 'mod_wsgi' in sys.modules: 
-        traceback.print_exc() 
-        os.kill(os.getpid(), signal.SIGINT) 
-        time.sleep(2.5) 
+"""
+WSGI config for demo_site project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qbit.settings")
+
+application = get_wsgi_application()
